047e9e1996f4db1788384f4b973f92bad6fc606d
joshuagordon <joshuagordon311@gmail.com>
2020-12-13 22:32:30 +1000

    Added listeners for dropping or picking up items. Cancelled for players in vanish.

--- src/main/java/dev/joshuagordon/myvanish/events/ItemEvents.java
+++ src/main/java/dev/joshuagordon/myvanish/events/ItemEvents.java
@@ -1,13 +1,41 @@
 package dev.joshuagordon.myvanish.events;
 
 import dev.joshuagordon.myvanish.MyVanish;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDropItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class ItemEvents implements Listener {
 
     private final MyVanish plugin;
+    public ItemEvents (MyVanish instance) { plugin = instance; }
 
-    public ItemEvents (MyVanish instance) {
+    @EventHandler
+    // Listen for Entity Item Pickup Event
+    public void onPickup(EntityPickupItemEvent event) {
 
+        // Check if entity is player
+        if (event.getEntity() instanceof Player) {
+            // Cast entity to player
+            Player player = (Player) event.getEntity();
+
+            // Cancel event for vanished players
+            if (plugin.getVanishedPlayers().contains(player))
+                event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    // Listen for Entity Item Drop Event
+    public void onDrop(PlayerDropItemEvent event) {
+
+        Player player = event.getPlayer();
+
+        // Cancel event for vanished players
+        if (plugin.getVanishedPlayers().contains(player))
+            event.setCancelled(true);
     }
 }
