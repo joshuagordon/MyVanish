32c2ee46424a3644636e8db72ba5e798ed8259f8
joshuagordon <joshuagordon311@gmail.com>
2020-12-14 06:35:12 +1000

    Tidied code and added more comments.

--- src/main/java/dev/joshuagordon/myvanish/events/JoinEvents.java
+++ src/main/java/dev/joshuagordon/myvanish/events/JoinEvents.java
@@ -1,6 +1,7 @@
 package dev.joshuagordon.myvanish.events;
 
 import dev.joshuagordon.myvanish.MyVanish;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,12 +10,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinEvents implements Listener {
 
     private final MyVanish plugin;
-
     public JoinEvents(MyVanish instance) { plugin = instance; }
 
     @EventHandler
+    // Listen for Player Join Event
     public void onJoin(PlayerJoinEvent event) {
 
+        // Store player in variable
         Player player = event.getPlayer();
 
         // Hide vanished players from newly joined players
